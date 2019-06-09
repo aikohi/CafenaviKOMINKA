fe9f25bcab3301ea83c0272c1df001a4c4405dd7
aikohi <aiko.t04.oar@gmail.com>
2019-06-09 09:32:26 +0000

    add/toppage/image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
