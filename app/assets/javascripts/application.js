74193d3f23bd1203d0cc08756fdf385f8e998fe6
aikohi <aiko.t04.oar@gmail.com>
2019-06-14 09:08:46 +0000

    ハンバーガー設定修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,22 +12,23 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(function () {
-	
-	$('.zdo_drawer_button').click(function () {
-		$(this).toggleClass('active');
-		$('.zdo_drawer_bg').fadeToggle();
-		$('nav').toggleClass('open');
+$(document).on('turbolinks:load',function() {
+	$(function () {
+		$('.zdo_drawer_button').click(function () {
+			$(this).toggleClass('active');
+			$('.zdo_drawer_bg').fadeToggle();
+			$('nav').toggleClass('open');
+		})
+		$('.zdo_drawer_bg').click(function () {
+			$(this).fadeOut();
+			$('.zdo_drawer_button').removeClass('active');
+			$('nav').removeClass('open');
+		});
 	})
-	$('.zdo_drawer_bg').click(function () {
-		$(this).fadeOut();
-		$('.zdo_drawer_button').removeClass('active');
-		$('nav').removeClass('open');
-	});
-})
+});
